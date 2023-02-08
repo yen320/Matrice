e4351041fb67eebb5472a6ce616197af6d55d08a
yen320 <guillodgaspard3@gmail.com>
2023-02-08 14:37:04 -0500

    retirer les fonction pk ca marche pas

---
+++ creation_matriceA&B.py
@@ -0,0 +1,41 @@
+    # taille de la matrice 
+nxma = input("taille de la matrice A ( nm , entre 1 et 9) : ")
+nxma.split(" ")
+print(nxma)
+na = nxma[0]
+ma = nxma[1]
+print(na)
+print(ma)
+
+    # deffinition de la matrice
+matriceA = []
+x = 1
+for i in range(int(na)):
+    ligne = input("ligne {} : (1,2,3,4,5, ... )".format(x))
+    ligne.split(",")
+    matriceA.append(ligne)
+    x += 1
+print("A = {}".format(matriceA))
+    # Matrice "A" créé
+
+
+    # taille de la matrice 
+nxmb = input("taille de la matrice B ( nm , entre 1 et 9) : ")
+nxmb.split(" ")
+print(nxmb)
+nb = nxmb[0]
+mb = nxmb[1]
+print(nb)
+print(mb)
+
+    # deffinition de la matrice
+matriceB = []
+x = 1
+for i in range(int(nb)):
+    ligne = input("ligne {} : (1,2,3,4,5, ... )".format(x))
+    ligne.split(",")
+    matriceB.append(ligne)
+    x += 1
+print("B = {}".format(matriceB))
+    # Matrice "B" créé
+
--- main.py
+++ main.py
@@ -1,51 +1,5 @@
 
 
-def fonctionmatriceA():
-    # taille de la matrice 
-    nxma = input("taille de la matrice A ( nm , entre 1 et 9) : ")
-    nxma.split(" ")
-    print(nxma)
-    na = nxma[0]
-    ma = nxma[1]
-    print(na)
-    print(ma)
-
-    # deffinition de la matrice
-    matriceA = []
-    x = 1
-    for i in range(int(na)):
-        ligne = input("ligne {} : (1,2,3,4,5, ... )".format(x))
-        ligne.split(",")
-        matriceA.append(ligne)
-        x += 1
-    print("A = {}".format(matriceA))
-    # Matrice "A" créé
-
-
-
-
-def fonctionmatriceB():
-    # taille de la matrice 
-    nxmb = input("taille de la matrice B ( nm , entre 1 et 9) : ")
-    nxmb.split(" ")
-    print(nxmb)
-    nb = nxmb[0]
-    mb = nxmb[1]
-    print(nb)
-    print(mb)
-
-    # deffinition de la matrice
-    matriceB = []
-    x = 1
-    for i in range(int(nb)):
-        ligne = input("ligne {} : (1,2,3,4,5, ... )".format(x))
-        ligne.split(",")
-        matriceB.append(ligne)
-        x += 1
-    print("B = {}".format(matriceB))
-    # Matrice "B" créé
-
-
 matriceC = []
 
 # choix de l'oppération 
@@ -57,8 +11,46 @@ opération = input(">")
 if opération == "1" :
     print("Attention aux régles des matrices !")
     print("l'oppération est A+B")
-    fonctionmatriceA()
-    fonctionmatriceB()
+
+        # taille de la matrice 
+    nxma = input("taille de la matrice A ( nm , entre 1 et 9) : ")
+    nxma.split(" ")
+    print(nxma)
+    na = nxma[0]
+    ma = nxma[1]
+    print(na)
+    print(ma)
+
+        # deffinition de la matrice
+    matriceA = []
+    x = 1
+    for i in range(int(na)):
+        ligne = input("ligne {} : (1,2,3,4,5, ... )".format(x))
+        ligne.split(",")
+        matriceA.append(ligne)
+        x += 1
+    print("A = {}".format(matriceA))
+        # Matrice "A" créé
+    
+        # taille de la matrice 
+    nxmb = input("taille de la matrice B ( nm , entre 1 et 9) : ")
+    nxmb.split(" ")
+    print(nxmb)
+    nb = nxmb[0]
+    mb = nxmb[1]
+    print(nb)
+    print(mb)
+
+        # deffinition de la matrice
+    matriceB = []
+    x = 1
+    for i in range(int(nb)):
+        ligne = input("ligne {} : (1,2,3,4,5, ... )".format(x))
+        ligne.split(",")
+        matriceB.append(ligne)
+        x += 1
+    print("B = {}".format(matriceB))
+        # Matrice "B" créé
 
     # verification que les dimmentions sont les memes 
     if na != nb or ma != mb :
@@ -76,12 +68,69 @@ if opération == "1" :
 elif opération == "2" :
     print("Attention aux régles des matrices !")
     print("l'oppération est AxB")
-    fonctionmatriceA()
-    fonctionmatriceB()
+
+        # taille de la matrice 
+    nxma = input("taille de la matrice A ( nm , entre 1 et 9) : ")
+    nxma.split(" ")
+    print(nxma)
+    na = nxma[0]
+    ma = nxma[1]
+    print(na)
+    print(ma)
+
+        # deffinition de la matrice
+    matriceA = []
+    x = 1
+    for i in range(int(na)):
+        ligne = input("ligne {} : (1,2,3,4,5, ... )".format(x))
+        ligne.split(",")
+        matriceA.append(ligne)
+        x += 1
+    print("A = {}".format(matriceA))
+        # Matrice "A" créé
+
+
+        # taille de la matrice 
+    nxmb = input("taille de la matrice B ( nm , entre 1 et 9) : ")
+    nxmb.split(" ")
+    print(nxmb)
+    nb = nxmb[0]
+    mb = nxmb[1]
+    print(nb)
+    print(mb)
+
+        # deffinition de la matrice
+    matriceB = []
+    x = 1
+    for i in range(int(nb)):
+        ligne = input("ligne {} : (1,2,3,4,5, ... )".format(x))
+        ligne.split(",")
+        matriceB.append(ligne)
+        x += 1
+    print("B = {}".format(matriceB))
+        # Matrice "B" créé
 
 elif opération == "3" :
     print("3")
-    fonctionmatriceA()
+        # taille de la matrice 
+    nxma = input("taille de la matrice A ( nm , entre 1 et 9) : ")
+    nxma.split(" ")
+    print(nxma)
+    na = nxma[0]
+    ma = nxma[1]
+    print(na)
+    print(ma)
+
+        # deffinition de la matrice
+    matriceA = []
+    x = 1
+    for i in range(int(na)):
+        ligne = input("ligne {} : (1,2,3,4,5, ... )".format(x))
+        ligne.split(",")
+        matriceA.append(ligne)
+        x += 1
+    print("A = {}".format(matriceA))
+        # Matrice "A" créé
 
 else :
     print("wrong number")
