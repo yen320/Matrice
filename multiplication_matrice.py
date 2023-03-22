d042f185e7417c8fd5a660678742386d8a32617c
yen320 <guillodgaspard3@gmail.com>
2023-03-21 20:37:56 -0400

    initialisation multiplication

--- multiplication_matrice.py
+++ multiplication_matrice.py
@@ -1,6 +1,22 @@
+na = 2
+ma = 2
+nb = 2
+mb = 2
+
+nc = na
+mc = mb 
+
 matriceA = [[0,2], [3,4]]
 matriceB = [[3,2], [4,4]]
 matriceC = []
 
+# verification :
 
+if na != mb or ma != nb :
+    print("opération impossible")
+    quit()
+
+matriceC
+
+print(matriceA[0][1])
 
