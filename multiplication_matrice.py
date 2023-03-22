a709781006d655017e0657c6d5a1cef165994221
yen320 <guillodgaspard3@gmail.com>
2023-03-21 21:06:00 -0400

    typo

--- multiplication_matrice.py
+++ multiplication_matrice.py
@@ -20,6 +20,7 @@ for i in range(nc):
     ligne = [0] * mc 
     matriceC.append(ligne)
 
+# calcules écrit à la main d'une matrice 2x2 (pas ouf) => à automatiser
 matriceC[0][0] = matriceA[0][0] * matriceB[0][0] + matriceA[0][1] * matriceB[1][0]
 matriceC[0][1] = matriceA[0][0] * matriceB[0][1] + matriceA[0][1] * matriceB[1][1]
 matriceC[1][0] = matriceA[1][0] * matriceB[0][0] + matriceA[1][1] * matriceB[1][0]
