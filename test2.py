a1836967d78db604a0d9bcd507b1b3b0453a1f7e
yen320 <guillodgaspard3@gmail.com>
2023-03-31 19:15:09 -0400

    typo + verif + précisions

--- test2.py
+++ test2.py
@@ -1,12 +1,22 @@
+# donées
 matriceA =[[1,2,3],[4,5,6],[7,8,9]]
 matriceB =[[9,2,1],[3,8,7],[0,1,4]]
 matriceC =[[0,0,0],[0,0,0],[0,0,0]]
 
+na = 3
+ma = 3
+nb = 3
+mb = 3
 nc = 3
 mc = 3
 
-n = 0
-m = 0
+# verif
+if na != mb or ma != nb :
+    print("! dimentions non compatibles !")
+    quit()
+
+# début du calcule 
+n = 0 
 for i in range(3):
     m = 0
     for i in range(3):
